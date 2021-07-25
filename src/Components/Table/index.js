81afabb11ec763506e89b00b152dca85d9e11188
kemo12 <dodkamel919@gmail.com>
2021-07-25 13:41:31 +0300

    site structure

---
+++ src/Components/Table/index.js
@@ -0,0 +1,13 @@
+import { Calendar } from 'antd'
+import React from 'react'
+
+const Table= () => {
+    return (
+        <div>
+            <Calendar/>
+        </div>
+    )
+}
+
+export default Table
+
