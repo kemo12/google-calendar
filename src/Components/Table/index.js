5c81e63b7ebaa27c7b36c0903412f8d933ce7fe8
kemo12 <dodkamel919@gmail.com>
2021-07-28 10:03:08 +0300

    day two done/make the list wtich sotre notice responsive

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -1,45 +1,53 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import { Calendar, Badge } from 'antd';
 import calendarContext from '../Context/Context';
 
 import "./Table.css";
+import moment from 'moment';
 const Table= () => {
 
+    // eslint-disable-next-line no-unused-vars
+    const [noticeList,setNoticeList]=useState(
+      [
+      {
+        date:moment(new Date() ),
+        notice:[
+          { type: 'warning', content: 'This is warning event' },
+          { type: 'success', content: 'This is very long usual event。。....' },
+          { type: 'error', content: 'This is error event 1.' },
+
+         ]
+      },
+      {
+        date:moment(new Date() ).add(1,'month'),
+        notice:[
+          { type: 'warning', content: 'This is warning event' },
+          { type: 'success', content: 'This is very long usual event。。....' },
+          { type: 'error', content: 'This is error event 1.' },
+          { type: 'error', content: 'This is error event 2.' },
+          { type: 'error', content: 'This is error event 3.' },
+          { type: 'error', content: 'This is error event 4.' },
+         ]
+      }
+    ]
+    ); 
     const value = useContext(calendarContext);
 
     const selectday=(day)=>{
       value.setDate.setDate(day)
     }
     
-    function getListData(value) {
+    
+    const getListData=(value)=> {
         let listData;
-        switch (value.date()) {
-          case 8:
-            listData = [
-              { type: 'warning', content: 'This is warning event.' },
-              { type: 'success', content: 'This is usual event.' },
-            ];
-            break;
-          case 10:
-            listData = [
-              { type: 'warning', content: 'This is warning event.' },
-              { type: 'success', content: 'This is usual event.' },
-              { type: 'error', content: 'This is error event.' },
-            ];
-            break;
-          case 15:
-            listData = [
-              { type: 'warning', content: 'This is warning event' },
-              { type: 'success', content: 'This is very long usual event。。....' },
-              { type: 'error', content: 'This is error event 1.' },
-              { type: 'error', content: 'This is error event 2.' },
-              { type: 'error', content: 'This is error event 3.' },
-              { type: 'error', content: 'This is error event 4.' },
-            ];
-            break;
-          default:
-        }
-        return listData || [];
+        console.log(noticeList)
+        noticeList.map((day)=>{
+         if(day.date.isSame(value,'year')&&day.date.isSame(value,'day')&&day.date.isSame(value, 'month')){
+           listData=day.notice
+         }
+        })   
+           return listData || []; 
+
       }
 
       function dateCellRender(value) {
